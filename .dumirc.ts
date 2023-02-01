bc8168bd665e722820cad71bac3df1b0b3064cb5
fxss <723107234@qq.com>
2023-02-01 22:48:14 +0800

    feat: 设置代码块样式

--- .dumirc.ts
+++ .dumirc.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'dumi';
 export default defineConfig({
   base: '/react-antd-docs',
   publicPath: '/react-antd-docs/',
+  styles: [`.markdown .dumi-default-source-code pre.prism-code { background-color: #ecf4fa; padding: 1em 1em 1em 3.5em; } .markdown .dumi-default-source-code pre.prism-code .token-line { display: list-item; list-style: decimal; border-left: 1px solid #d8e9f6; padding-left: 1em; }`],
   themeConfig: {
     name: 'react-antd',
     nav: [
