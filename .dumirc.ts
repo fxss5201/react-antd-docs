f3bb2e18586cdb4ae2a093efa4f008328fd010fe
fanyushun <fanyushun@zj.tech>
2023-02-02 10:08:11 +0800

    feat: 代码块颜色兼容暗色模式

--- .dumirc.ts
+++ .dumirc.ts
@@ -3,7 +3,12 @@ import { defineConfig } from 'dumi';
 export default defineConfig({
   base: '/react-antd-docs',
   publicPath: '/react-antd-docs/',
-  styles: [`.markdown .dumi-default-source-code pre.prism-code { background-color: #ecf4fa; padding: 1em 1em 1em 3.5em; } .markdown .dumi-default-source-code pre.prism-code .token-line { display: list-item; list-style: decimal; border-left: 1px solid #d8e9f6; padding-left: 1em; }`],
+  styles: [`
+    .markdown .dumi-default-source-code pre.prism-code { background-color: #ecf4fa; padding: 1em 1em 1em 3.5em; }
+    [data-prefers-color=dark] .markdown .dumi-default-source-code pre.prism-code { background-color: #282c34; }
+    .markdown .dumi-default-source-code pre.prism-code .token-line { display: list-item; list-style: decimal; border-left: 1px solid #d8e9f6; padding-left: 1em; }
+    [data-prefers-color=dark] .markdown .dumi-default-source-code pre.prism-code .token-line { border-left: 1px solid #222935; }
+  `],
   themeConfig: {
     name: 'react-antd',
     nav: [
